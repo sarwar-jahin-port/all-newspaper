692901aed6f603cab75c56df97a400a80903eac9
sarwar-jahin-port <sarwarjahinpersonal@gmail.com>
2023-12-27 17:58:23 +0600

    categorized alphabetically.A, B countries are added.

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,18 @@ import './App.css'
 import { Countries } from './components/Countries/Countries'
 
 function App() {
-
+  let letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   return (
     <>
       <h1 className='text-5xl text-center mb-10'>Select a country to see newspapers</h1>
-      <Countries></Countries>
+      {
+        letters.map((letter, index) => (
+          <div key={index}>
+            <h2 className='mx-auto w-4/5 text-2xl'>{letter}</h2>
+            <Countries letter={letter}></Countries>
+          </div>
+        ))
+      }
     </>
   )
 }
--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 
-export const Countries = () => {
+export const Countries = ({letter}) => {
     const [countries, setCountries] = useState([]);
     useEffect(()=>{
         fetch("countries.json")
@@ -8,15 +8,22 @@ export const Countries = () => {
         .then(data => setCountries(data))
     }, [])
   return (
-    <div className='flex gap-5 w-4/5 mx-auto'>
+    <>
+    <div className='flex flex-wrap gap-5 w-4/5 mx-auto mb-10'>
         {
-            countries.map((country, index) => (
-                <div key={index} className="country border">
-                    <img src={country.flag} alt="" />
-                    <p className='text-center'>{country.name}</p>
-                </div>
-            ))
+            countries.map((country, index) => {
+                console.log("here");
+                if(country.name[0].toLowerCase()===letter.toLowerCase()){
+                    return(
+                    <div key={index} className="country border">
+                        <img src={country.flag} alt="" />
+                        <p className='text-center'>{country.name}</p>
+                    </div>
+                    )
+                }
+            })
         }
     </div>
+    </>
   )
 }
