1e86ef395c8731f639f69f43f109a816f58c895e
sarwar-jahin-port <sarwarjahinpersonal@gmail.com>
2024-01-03 07:00:43 -0800

    collected data and formatted for the first test a countries newspaper data and some simple modifications

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,16 +4,47 @@ import { Countries } from '../../components/Countries/Countries';
 export const Home = () => {
 let letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "Y", "Z"];
   return (
-    <div>
-        <h1 className='text-5xl text-center mb-10'>Select a country to see newspapers</h1>
-      {
-        letters.map((letter, index) => (
-          <div key={index}>
-            <h2 className='mx-auto w-4/5 text-2xl'>{letter}</h2>
-            <Countries letter={letter}></Countries>
-          </div>
-        ))
-      }
+    <div className='flex border'>
+      <div className='magazines'>
+        <ul>
+          <li>African American</li>
+          <li>Arts</li>
+          <li>Business</li>
+          <li>Cars</li>
+          <li>Computers</li>
+          <li>Cooking</li>
+          <li>Education</li>
+          <li>Entertainment</li>
+          <li>Fashion</li>
+          <li>Health</li>
+          <li>History</li>
+          <li>Home</li>
+          <li>Kids</li>
+          <li>Literature</li>
+          <li>Men's</li>
+          <li>Music</li>
+          <li>News</li>
+          <li>Photography</li>
+          <li>Science</li>
+          <li>Spanish</li>
+          <li>Sports</li>
+          <li>Teen</li>
+          <li>Travel</li>
+          <li>Women's</li>
+        </ul>
+      </div>
+      <div>
+          <h1 className='text-5xl text-center'>Select a country to see newspapers</h1>
+        {
+          letters.map((letter, index) => (
+            <div key={index}>
+              <h2 className='mx-auto w-4/5 text-2xl'>{letter}</h2>
+              <Countries letter={letter}></Countries>
+            </div>
+          ))
+        }
+      </div>
+      <div></div>
     </div>
   )
 }
--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -2,6 +2,27 @@ import React from 'react'
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+      <div className="site-title flex w-4/5 mx-auto justify-between items-center">
+        <a href=""><img src="" alt="" className="logo" />Logo</a>
+        <div className='flex flex-col'>
+          <h1 className='text-4xl'>World Newspapers, News Sites, and Magazines Online</h1>
+          <div className="continents text-center">
+            <a href="/" className="continent ml-2">Africa |</a>
+            <a href="/" className="continent ml-2"> Asia |</a>
+            <a href="/" className="continent ml-2"> Caribbean |</a>
+            <a href="/" className="continent ml-2"> Central America |</a>
+            <a href="/" className="continent ml-2"> Europe |</a>
+            <a href="/" className="continent ml-2"> North America |</a>
+            <a href="/" className="continent ml-2"> Oceania |</a>
+            <a href="/" className="continent ml-2"> South America</a>
+          </div>
+        </div>
+        <div className="search">
+          <input className='border' type="text" name="search" id="search" />
+          <button>Search</button>
+        </div>
+      </div>
+    </div>
   )
 }
