eb3c172c4f9a93aefed19317783c4849a898dc1f
sarwar-jahin-port <sarwarjahinpersonal@gmail.com>
2024-01-14 03:33:00 -0800

    newspaper page structure is confirmed.All A country links and data are successfully visible.

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -13,9 +13,7 @@ export const Countries = ({letter}) => {
     <div className='grid grid-cols-4 gap-5 w-4/5 mx-auto mb-10'>
         {
             countries.map((country, index) => {
-                console.log("here");
                 if(country.name[0].toLowerCase()===letter.toLowerCase()){
-                    console.log(country);
                     return(
                     <div key={index} className="country flex items-center gap-2">
                         <img className='w-8 h-5' src={country.flag} alt="" />
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
-import { useLoaderData, useParams } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom'
 
 export const Country = () => {
-    const {id} = useParams();
-    const data = useLoaderData();
-    console.log(data);
+  const {id} = useParams();
+  const [country, setCountry] = useState({});
+  
+  useEffect(()=>{
+      fetch("all-a-country-data.json")
+      .then(res => res.json())
+      .then(data => setCountry(data.filter(c => c["country-name"] === id)[0]))
+  }, [])
+  // country["newspapers"].map(newspaper => console.log(newspaper));
   return (
-    <div>{id}</div>
+    country["newspapers"] &&(
+      country["newspapers"].map((newspaper, index) => (<div key={index} className='w-4/5 mx-auto mb-5'>
+        <Link to={newspaper["newspaper-link"]} className='text-2xl font-bold text-blue-600 hover:underline'><p>{newspaper["newspaper-name"]}</p></Link>
+        <p className='text-xl'>{newspaper["newspaper-about"]}</p>
+      </div>
+      ))
+    )
   )
 }
